02c20c706e6d60edfc3cff3aff7956b66c44218b
Sahan Thinusha <sahan@virtuan.io>
2023-05-06 13:45:17 +0530

    u

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def predict_repair_time():
     return jsonify({'estimated_repair_time': estimated_repair_time})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8082)
+app.run(host='0.0.0.0', port=8082)
